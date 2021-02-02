085c1ccd4e84ebec2577b584a74a289124fdcfe4
Nguyen Tien Dat Tran <t.ng.t.dat@gmail.com>
2021-02-02 11:39:35 +0100

    Clean code

--- .dma_for_IPCORE_2.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stud/nt92homu/echo2/task3/Accelerators-for-systems-on-chip-SoC-/dma_for_IPCORE_2.mod := { echo  /home/stud/nt92homu/echo2/task3/Accelerators-for-systems-on-chip-SoC-/dma_for_IPCORE_2.o; echo; } > /home/stud/nt92homu/echo2/task3/Accelerators-for-systems-on-chip-SoC-/dma_for_IPCORE_2.mod
